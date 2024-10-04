38ee683d7b26bc9e137cd676c62439cfbd379491
Tobias Thorin <tobias.thorin@imbox.se>
2024-10-04 18:43:27 +0200

    handle signed in/out user on public pages

--- src/middleware/sessionValidation.ts
+++ src/middleware/sessionValidation.ts
@@ -6,7 +6,7 @@ import { useNamespace, USERS_CACHE } from "../lib/cache"
 import type { APIContext, MiddlewareNext } from "astro"
 
 const protectedRoutes = ["/profile/**", "/api/favourite/**", "/api/event/**"]
-// const publicRoutes = ["/events/**", "/map", "/register(|/)"]
+const publicRoutes = ["/events/**", "/map"]
 
 const usersCache = useNamespace<UserDetails>(USERS_CACHE)
 
@@ -22,62 +22,60 @@ export async function sessionValidation(
     log(`Accessing unprotected route: ${url.pathname}`)
   }
 
-  if (micromatch.isMatch(url.pathname, protectedRoutes)) {
+  if (micromatch.isMatch(url.pathname, [...protectedRoutes, ...publicRoutes])) {
     const accessToken = cookies.get("sb-access-token")
     const refreshToken = cookies.get("sb-refresh-token")
 
     const isSignedOut = !accessToken || !refreshToken
-
     if (isSignedOut) {
       locals.isSignedIn = false
-      return redirect("/events")
-    } else {
-      locals.isSignedIn = true
-    }
 
-    const { data: sessionData, error } = await supabase.auth.setSession({
-      refresh_token: refreshToken.value,
-      access_token: accessToken.value,
-    })
-
-    let userDetails = usersCache.get(sessionData.user?.id!)
-
-    if (!userDetails) {
-      log(`Getting user details for ${sessionData.user?.id!} from DB.`)
-
-      const { data: userData, error: userError } = await getUserDetails(
-        sessionData.user?.id!
-      )
-
-      if (error || userError) {
-        cookies.delete("sb-access-token", {
-          path: "/",
-        })
-        cookies.delete("sb-refresh-token", {
-          path: "/",
-        })
+      if (!micromatch.isMatch(url.pathname, publicRoutes)) {
         return redirect("/events")
       }
+    } else {
+      locals.isSignedIn = true
 
-      userDetails = userData!
+      const { data: sessionData, error } = await supabase.auth.setSession({
+        refresh_token: refreshToken.value,
+        access_token: accessToken.value,
+      })
+
+      let userDetails = usersCache.get(sessionData.user?.id!)
+
+      if (!userDetails) {
+        log(`Getting user details for ${sessionData.user?.id!} from DB.`)
+
+        const { data: userData, error: userError } = await getUserDetails(
+          sessionData.user?.id!
+        )
+
+        if (error || userError) {
+          cookies.delete("sb-access-token", { path: "/" })
+          cookies.delete("sb-refresh-token", { path: "/" })
+          return redirect("/events")
+        }
+
+        userDetails = userData!
+      }
+
+      locals.user = {
+        id: sessionData.user?.id!,
+        email: sessionData.user?.email!,
+        details: userDetails,
+      }
+
+      cookies.set("sb-access-token", sessionData?.session?.access_token!, {
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      })
+      cookies.set("sb-refresh-token", sessionData?.session?.refresh_token!, {
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      })
     }
-
-    locals.user = {
-      id: sessionData.user?.id!,
-      email: sessionData.user?.email!,
-      details: userDetails,
-    }
-
-    cookies.set("sb-access-token", sessionData?.session?.access_token!, {
-      sameSite: "lax",
-      path: "/",
-      secure: true,
-    })
-    cookies.set("sb-refresh-token", sessionData?.session?.refresh_token!, {
-      sameSite: "lax",
-      path: "/",
-      secure: true,
-    })
   }
 
   return next()
