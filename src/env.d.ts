589cc3b5f43ce0a5b981fd4ee28d8c7076da47fe
Tobias Thorin <tobias.thorin@imbox.se>
2024-10-15 17:38:38 +0200

    fancy markdown editor!

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,28 +1,38 @@
 /// <reference path="../.astro/types.d.ts" />
-interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_ANON_KEY: string
-  readonly DEBUG: "true" | undefined
-  readonly DEBUG_LOGS: "true" | undefined
-}
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
+import SimpleMDE from "@types/simplemde"
 
-declare const initFlowbite: () => void
-declare const htmx: {
-  onLoad: (fn: () => void) => void
-  process: (element: HTMLElement) => void
-}
+declare global {
+  interface ImportMetaEnv {
+    readonly SUPABASE_URL: string
+    readonly SUPABASE_ANON_KEY: string
+    readonly DEBUG: "true" | undefined
+    readonly DEBUG_LOGS: "true" | undefined
+  }
 
-declare namespace App {
-  interface Locals {
-    isSignedIn: boolean
-    user: {
-      id: string
-      email: string
-      details: import("./types/supabase").Tables<"user_details">
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
+
+  interface Window {
+    SimpleMDE: SimpleMDE
+  }
+
+  declare const initFlowbite: () => void
+
+  declare const htmx: {
+    onLoad: (fn: () => void) => void
+    process: (element: HTMLElement) => void
+  }
+
+  declare namespace App {
+    interface Locals {
+      isSignedIn: boolean
+      user: {
+        id: string
+        email: string
+        details: import("./types/supabase").Tables<"user_details">
+      }
     }
   }
 }
