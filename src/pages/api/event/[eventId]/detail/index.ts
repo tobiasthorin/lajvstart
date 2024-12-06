127324f92e6acff8feb5bce1689f142096699755
Tobias Thorin <tobias.thorin@imbox.se>
2024-12-06 11:59:38 +0100

    feat: add autofill options on event form controls

--- src/pages/api/event/[eventId]/detail/index.ts
+++ src/pages/api/event/[eventId]/detail/index.ts
@@ -6,6 +6,14 @@ import {
   type EventDetailsType,
 } from "../../../../../services/eventService"
 
+const autofillOptions = ["characterName", "characterDescription"] as const
+const autofillNameOptions = ["Character name", "Character description"] as const
+
+const autofillNames = new Map([
+  [autofillOptions[0], autofillNameOptions[0]],
+  [autofillOptions[1], autofillNameOptions[1]],
+])
+
 export const POST: APIRoute = async ({ request, params, rewrite }) => {
   const eventId = params.eventId
 
@@ -18,10 +26,22 @@ export const POST: APIRoute = async ({ request, params, rewrite }) => {
     | undefined
   const controlName = formData.get("controlName")?.toString()
   const controlDescription = formData.get("controlDescription")?.toString()
+  let autofillWith = formData.get("autofillWith")?.toString() as
+    | (typeof autofillOptions)[number]
+    | undefined
 
   if (!controlType || !controlName) {
     return errorResponse("Missing form data")
   }
+
+  if (
+    !autofillWith ||
+    !autofillOptions.includes(autofillWith) ||
+    ["number", "checkbox", "multiChoice", "multiChoiceMultiValue"].includes(
+      controlType,
+    )
+  )
+    autofillWith = undefined
 
   const event = await getEvent(eventId)
 
@@ -32,6 +52,10 @@ export const POST: APIRoute = async ({ request, params, rewrite }) => {
       label: controlName,
       description: controlDescription,
       options: controlType === "multiChoice" ? [] : undefined,
+      autofillWith,
+      autofillWithName: autofillWith
+        ? autofillNames.get(autofillWith)
+        : undefined,
     })
   } catch (error) {
     console.error(error)
