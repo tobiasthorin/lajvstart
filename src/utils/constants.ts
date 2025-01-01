46a0fb40c8ac27083e29fa13e9be77f0e32c5594
Tobias Thorin <tobias.thorin@imbox.se>
2025-01-01 18:49:57 +0100

    feat: password reset

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -16,5 +16,12 @@ export const eventTags = [
 ] as const
 
 export const isDev = import.meta.env.DEV_BUILD
-export const isProd = !isDev && import.meta.env.PROD;
-export const isLocal = import.meta.env.DEV;
+export const isProd = !isDev && import.meta.env.PROD
+export const isLocal = import.meta.env.DEV
+
+let baseURL = ""
+if (isLocal) baseURL = "http://localhost:4321"
+if (isDev) baseURL = "https://dev.lajvstart.se"
+if (isProd) baseURL = "https://www.lajvstart.se"
+
+export { baseURL }
