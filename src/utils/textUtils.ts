d1cd64fb7b9bd42d5f53143ec57b0621a15fa897
Tobias Thorin <tobias.thorin@imbox.se>
2025-03-12 17:42:47 +0100

    feat: pretty url

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -1,3 +1,10 @@
 export function escapeCharacters(text: string) {
   return text.replace(/\"/g, "&quot;")
 }
+
+export function prettifyURL(url: string) {
+  return url
+    .trim()
+    .replace(/^https*:\/\//, "")
+    .replace(/\/$/, "")
+}
