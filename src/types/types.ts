9ffa4f9e7311c52623fbc9118d9b73f5c2c49c61
Tobias Thorin <tobias.thorin@imbox.se>
2025-03-30 17:08:29 +0200

    update event service with prices

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,10 @@
-import type { EventDetailsSchema } from "../services/eventService"
+import type { EventDetailsSchema, EventPrices } from "../services/eventService"
 import type { eventTags } from "../utils/constants"
 import type { Database } from "./supabase"
 
 export type LARPEvent = Database["public"]["Tables"]["events"]["Row"] & {
   details: EventDetailsSchema
+  prices: EventPrices
 }
 
 export type EventTag = (typeof eventTags)[number]
